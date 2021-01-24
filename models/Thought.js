e1dd92c3fbc816d3e9ac6bbd01d75b087d5fe4fe
Chris Wein <chriswein12@yahoo.com>
2021-01-24 03:29:58 -0600

    Added the Thought and Reaction Schemas

---
+++ models/Thought.js
@@ -0,0 +1,63 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+const ReactionsSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 1
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format("MMM Do YYYY, h:mm:ss a")
+        }
+    }
+)
+
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format("MMM Do YYYY, h:mm:ss a")
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [ReactionsSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
+
