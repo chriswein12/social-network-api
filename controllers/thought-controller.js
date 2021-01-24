1915ee48278c6eae733f3710e4cefd1425e0519c
Chris Wein <chriswein12@yahoo.com>
2021-01-24 04:30:28 -0600

    All method types added to controller files

---
+++ controllers/thought-controller.js
@@ -0,0 +1,34 @@
+const { User, Thought } = require('../models');
+
+const thoughtController = {
+
+    getAllThoughts() {
+
+    },
+
+    getThoughtById() {
+
+    },
+
+    createThought() {
+
+    },
+
+    updateThought() {
+
+    },
+
+    deleteThought() {
+
+    },
+    
+    createReaction() {
+
+    },
+
+    deleteReaction() {
+
+    }
+}
+
+module.exports = thoughtController;
---
+++ controllers/user-controller.js
@@ -0,0 +1,35 @@
+const { User } = require('../models');
+
+const userController = {
+
+    getAllUsers() {
+
+    },
+
+    getUserById() {
+
+    },
+
+    createUser() {
+
+    },
+
+    updateUser() {
+
+    },
+
+    deleteUser() {
+
+    },
+
+    addFriend() {
+
+    },
+
+    deleteFriend() {
+
+    }
+
+}
+
+module.exports = userController;
